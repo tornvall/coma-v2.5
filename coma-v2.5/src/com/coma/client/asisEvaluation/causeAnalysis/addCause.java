145f26269a09d21b6d277c947b32ace121ecc0b9
tornvall <f.tornvall@gmail.com>
2014-05-24 06:54:35 +0200

    package for As-Is Evaluation

---
+++ coma-v2.5/src/com/coma/client/asisEvaluation/causeAnalysis/addCause.java
@@ -0,0 +1,5 @@
+package com.coma.client.asisEvaluation.causeAnalysis;
+
+public class addCause {
+
+}
---
+++ coma-v2.5/src/com/coma/client/asisEvaluation/problemsOpportunities/addChangeProblems.java
@@ -0,0 +1,5 @@
+package com.coma.client.asisEvaluation.problemsOpportunities;
+
+public class addChangeProblems {
+
+}
